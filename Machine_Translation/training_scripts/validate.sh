bc2c5e2f8e72ee696aaa6688bd3e95169b9774de
Martina Nyberg <83071382+MartinaNyberg@users.noreply.github.com>
2022-09-05 12:58:52 +0200

    Update validate.sh

--- Machine_Translation/training_scripts/validate.sh
+++ Machine_Translation/training_scripts/validate.sh
@@ -1,4 +1,4 @@
-cat $1 | sed 's/\@\@ //g' > /proj/uppmax2021-2-31/martina/data/trans.out
-python3 /proj/uppmax2021-2-31/martina/scripts/compute_gleu.py -r /proj/uppmax2021-2-31/martina/data/dev.bpe.cor -s /proj/uppmax2021-2-31/martina/data/dev.bpe.err \
-    -o /proj/uppmax2021-2-31/martina/data/trans.out
+cat $1 | sed 's/\@\@ //g' > PATH_TO_TRANS_OUTPUT/trans.out
+python3 PATH_TO_GLEU_SCRIPTS/compute_gleu.py -r PATH_TO_DEV_DATA/dev.bpe.cor -s PATH_TO_DEV_DATA/dev.bpe.err \
+    -o OUTPUT_PATH/trans.out
 
